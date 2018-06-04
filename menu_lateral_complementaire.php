c5faa11535c308d2f792221f1874533d424aa96e
Yannick Betemps <yannick@alternetic.com>
2018-06-04 15:25:00 +0200

    mise en forme avec menu horizontalisé dans la recherche

--- menu_lateral_complementaire.php
+++ menu_lateral_complementaire.php
@@ -4,7 +4,7 @@
 			<li role="menu" class="dropdown">
 				<a href="index.php" title="Rubrique gestion de l'eau" class="toRight">Consultation 2018</a>
 				<ul class="dropdown-menu" role="menu">
-					  <li><a href="index.php">Recherche de masses d'eau</a></li>
+					  <li class="recherche"><a href="index.php">Recherche de masses d'eau</a></li>
 					  <?php if ($auth->isLoaded()) { ?><li><a href="programme.php">Précisions et consignes</a></li><?php }?>
 					  <li><a href="connexion.php"><?php echo ($auth->isLoaded()?"Votre espace":"Déposer un avis"); ?></a></li>
 				</ul>
