5f0346eda158befb93cf6cbbecdcd53a87a9221f
Yannick Betemps <yannick@alternetic.com>
2018-06-08 17:04:51 +0200

    ajout d'une fonction de test de CSV

--- csvtest.php
+++ csvtest.php
@@ -3,11 +3,7 @@
 		<title>Test CSV vers table</title>
 	</head>
 	<body>
-		<form method="post" action="#" 
-							  onsubmit="return frontCtl.testFormulaireAvis(this);"
-							  id="formAvisPression_{id_pression}_{id_massedeau}" 
-							  enctype="multipart/form-data" target="targetSauvegarde"
-							  class="formAvisMassedeau">
+		<form method="post" action="#"  enctype="multipart/form-data" class="formAvisMassedeau">
 			<input type="file" name="documents" />
 			<input type="submit" name="test">
 		</form>
